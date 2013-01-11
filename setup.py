67bea51b0f60a82882ff030100cdc6fe90a6f376
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-01-11 15:27:51 +0100

    Added setup.py script to be able to run sdist

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='hyperion-dust',
+      version="0.1.0",
+      url='http://www.hyperion-rt.org',
+      author='Thomas Robitaille',
+      author_email='thomas.robitaille@gmail.com',
+      summary='Library of dust files for the Hyperion radiative transfer code'
+      packages=[],
+      license='BSD'
+     )
+
