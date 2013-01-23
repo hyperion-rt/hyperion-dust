af700af6efe8fe54980dcd3b7a7869e0126a6e03
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-01-23 14:59:25 +0100

    Added original files for refractive indices

---
+++ dust_models/ri-data/convert.py
@@ -0,0 +1,20 @@
+import glob
+import os
+
+import numpy as np
+
+def convert(file_in, file_out):
+    
+    data = np.loadtxt(file_in, skiprows=5)
+
+    wav = data[:,0]
+    real = data[:,3] + 1.
+    imag = data[:,4]
+    
+    np.savetxt(file_out, np.vstack([wav, real, imag]).transpose(), fmt="%.6e")
+    
+convert('callindex.out_silD03', 'silicate_d03')
+convert('callindex.out_CpaD03_0.01', 'graphite_par_d03_0.01')
+convert('callindex.out_CpaD03_0.10', 'graphite_par_d03_0.10')
+convert('callindex.out_CpeD03_0.01', 'graphite_per_d03_0.01')
+convert('callindex.out_CpeD03_0.10', 'graphite_per_d03_0.10')
